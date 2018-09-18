67e28ab80fa43c446d3e8dc18c322a59bfc747a5
guillewolox <guillermo.sequeira@wolox.com.ar>
2018-09-18 14:09:20 -0300

    test number 1 of promises

--- react/ecmascript/02 - promises/index.js
+++ react/ecmascript/02 - promises/index.js
@@ -1,9 +1,11 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
-
+export function delay(time) {
+  const wait = ms =>
+    new Promise((resolve, reject) => {
+      resolve(time);
+    });
+  return wait();
 }
 
-export function asyncDelay() {
-
-}
+export function asyncDelay() {}
