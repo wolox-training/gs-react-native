eff0727851433778f7f6031d6b26207feb0ec34a
guillewolox <guillermo.sequeira@wolox.com.ar>
2018-09-18 08:53:44 -0300

    first commit of test number 4

--- react/ecmascript/01 - spread/index.js
+++ react/ecmascript/01 - spread/index.js
@@ -1,17 +1,20 @@
 import isArray from './utils';
 
 export function min(...numbers) {
-  if (isArray(numbers[0])) {
-    return Math.min(...numbers[0]);
+  if (numbers.length !== 0) {
+    if (isArray(numbers[0])) {
+      return Math.min(...numbers[0]);
+    }
+    if (numbers.length === 1) {
+      return Math.min(numbers);
+    }
+    const array = [];
+    for (let i = 0; i < numbers.length; i++) {
+      array.push(numbers[i]);
+    }
+    return Math.min(...array);
   }
-  if (numbers.length === 1) {
-    return Math.min(numbers);
-  }
-  const array = [];
-  for (let i = 0; i < numbers.length; i++) {
-    array.push(numbers[i]);
-  }
-  return Math.min(...array);
+  return undefined;
 }
 
 export function copy() {}
