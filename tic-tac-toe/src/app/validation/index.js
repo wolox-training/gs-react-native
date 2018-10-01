948fe4cfa2a9dbaa0f8bb340f235aba72cae4eac
guillewolox <guillermo.sequeira@wolox.com.ar>
2018-10-01 16:43:28 -0300

    add regex of email and check of password

--- tic-tac-toe/src/app/validation/index.js
+++ tic-tac-toe/src/app/validation/index.js
@@ -1,13 +1,16 @@
 export const validate = values => {
   const errors = {};
-  if (!values.user) {
-    errors.user = 'usuario no valido';
-  } else if (values.user.length > 12) {
-    errors.user = 'usuario con demasiados caracteres';
+  const pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (values.user !== undefined) {
+    if (!pattern.test(values.user)) {
+      errors.user = 'usuario no valido';
+    }
   }
 
   if (!values.pass) {
     errors.pass = 'password no valida';
+  } else if (values.pass.length < 8) {
+    errors.user = 'password con menos de 8 caracteres';
   }
 
   return errors;
