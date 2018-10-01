09c8c18af74a6d475eddc33b4421519030b3a6e0
guillewolox <guillermo.sequeira@wolox.com.ar>
2018-10-01 08:52:51 -0300

    modify in TextInput

--- tic-tac-toe/src/app/components/textInput/index.js
+++ tic-tac-toe/src/app/components/textInput/index.js
@@ -1,14 +1,11 @@
 import React, { Fragment } from 'react';
 
-const TextInput = props => {
-  const { label, input, type, meta } = props;
-  return (
-    <Fragment>
-      <label htmlFor={input.name}>{label}</label>
-      <input {...input} type={type} />
-      {meta.error && meta.touched && <div style={{ color: 'red' }}>{meta.error}</div>}
-    </Fragment>
-  );
-};
+const TextInput = ({ label, input, type, meta }) => (
+  <Fragment>
+    <label htmlFor={input.name}>{label}</label>
+    <input {...input} type={type} />
+    {meta.error && meta.touched && <div style={{ color: 'red' }}>{meta.error}</div>}
+  </Fragment>
+);
 
 export default TextInput;
