ef161d3162c98e094b6c5b681626d6a6e8ce8f1f
guillewolox <guillermo.sequeira@wolox.com.ar>
2018-10-03 16:33:00 -0300

    fix validate user

--- tic-tac-toe/src/app/validation/loginValidation.js
+++ tic-tac-toe/src/app/validation/loginValidation.js
@@ -1,22 +1,17 @@
 export const validate = values => {
   const errors = {};
   const pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (values.user !== undefined) {
-    if (!pattern.test(values.user)) {
-      errors.user = 'usuario no valido';
-    }
-  } else {
-    values.user === undefined;
+
+  if (!values.user) {
+    errors.user = 'usuario no valido';
+  } else if (!pattern.test(values.user)) {
     errors.user = 'usuario no valido';
   }
-  if (values.pass !== undefined) {
-    if (!values.pass) {
-      errors.pass = 'password no valida';
-    } else if (values.pass.length < 8) {
-      errors.pass = 'password con menos de 8 caracteres';
-    }
-  } else {
+
+  if (!values.pass) {
     errors.pass = 'password no valida';
+  } else if (values.pass.length < 8) {
+    errors.pass = 'password con menos de 8 caracteres';
   }
 
   return errors;
